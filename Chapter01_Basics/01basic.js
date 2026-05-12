a7489153b2fa0a0d6c7afa2f5815ef86ab68571d
Akshay Kurle <akshay.23696@gmail.com>
2026-05-12 19:53:03 +0530

    Initial commit - add basic console.log examples

--- Chapter01_Basics/01basic.js
+++ Chapter01_Basics/01basic.js
@@ -3,4 +3,8 @@ console.log(a);
 
 console.log("hello the testing academy");
 
-console.log(Process.platform);
+console.log("Hi Akshay, how are you");
+
+console.log(process.platform);
+console.log(process.arch);
+console.log(process.version);
