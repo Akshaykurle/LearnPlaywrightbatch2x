58480e3262bf77a13d00c0a0e9e462eebf5bb726
Akshay Kurle <akshay.23696@gmail.com>
2026-06-11 22:19:14 +0530

    Add task_8thjune.js

---
+++ task_8thjune.js
@@ -0,0 +1,28 @@
+let n = 5;
+for (let i = n; i >= 1; i--) {
+    let row = " ";
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
+    console.log(row.trim());
+
+}
+
+
+
+let row = 5;
+
+for (let i = 1; i <= row; i++) 
+    {
+    let str = " ";
+    for (let j = 0; j <= row-1-i; j++)
+    {
+        str += " ";
+    }
+    for (let k = 0; k<i; k++) 
+    {
+        str += "* ";
+    }
+    console.log(str);
+
+    }
